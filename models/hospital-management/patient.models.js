122328b36f5ba8691cf706102f21ee36d66e916f
itakeutkarsh-cyber <itakeutkarsh@gmail.com>
2026-03-18 22:30:17 +0530

    updated models of order and hospital-management

---
+++ models/hospital-management/patient.models.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+const patientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    
+}, {timestamps: true})
+
+export const Patient = mongoose.model("Patient", patientSchema)
